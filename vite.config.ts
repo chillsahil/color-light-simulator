16df8036a045d418bb867fd8e7353fb8c5d8e3bd
Sahil <prustisahil@gmail.com>
2025-05-25 02:48:31 -0400

    chore: configure base path and homepage for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // When deploying under https://chillsahil.io/color-light-simulator/
   base: '/color-light-simulator/',
-  plugins: [react()],
+
+  plugins: [
+    react(),
+  ],
+
+  // (any other custom config you already had…)
 })
